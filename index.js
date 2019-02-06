0baf9e8c99f929583f526354e732dc26e6477465
geckoslair <enrico@piramid.studio>
2019-02-06 16:11:47 +0100

    finalizing workshop

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var Web3 = require('web3');
+var web3 = new Web3("ws://localhost:8546");
+var PiramidTokenOBJ = require('./build/contracts/PiramidToken.json')
+
+
+// viewed at http://localhost:8080
+app.use(express.static('examples'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/examples/index.html'));
+});
+
+var PiramidTokenContract = new web3.eth.Contract(PiramidTokenOBJ.abi, "0x2A2B68E9Bf718DBf6dF3dF91174A136054a8A0Ec");
+PiramidTokenContract.events.Transfer().on('data', function(event){
+  console.log(event);
+
+  //use mongo, mysql etc. to index the event
+
+})
+
+
+app.listen(8080);
